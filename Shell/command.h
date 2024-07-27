ed6bf641bfdec2d18581b191f87546da7777a351
sohila-mohamed22 <sohilamnasr@gmail.com>
2024-07-27 23:45:52 +0300

    Add files via upload

---
+++ Shell/command.h
@@ -0,0 +1,10 @@
+#ifndef PRINT_WORKING_DIRECTORY_H
+#define PRINT_WORKING_DIRECTORY_H
+char * trim_newline(char *str); 
+void print_working_directory();
+void print_user_input();
+void copy_files();
+void move_files();
+#endif // PRINT_WORKING_DIRECTORY_H
+
+
